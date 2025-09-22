f0df1e27e534744ec64f2048cf151363f1af1af4
aishaezzeldin <aishaashraf1000@gmail.com>
2025-09-23 00:07:57 +0300

    cookies name

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,8 +3,7 @@ import { cookies } from "next/headers";
 
 
  export async function GetUserToken(){
-    const TokenSession =( process.env.NODE_ENV === "production"?'__Secure-next-auth.session-token' : 'next-auth.session-token')
-
+   const TokenSession =( process.env.NODE_ENV === "production"?'__Secure-next-auth.session-token' : 'next-auth.session-token')
   const cookie = await cookies();
  const decodedToken =  cookie.get(TokenSession)?.value;
  const MyToken = await decode({
